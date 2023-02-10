d1e751158ee725af54504353d32f8132b8eba4be
anshorikasri <anshorikasri.4@gmail.com>
2023-02-11 03:18:52 +0700

    pertama

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoute from "./routes/UserRoute.js";
+
+const app = express();
+mongoose.set('strictQuery', true)
+mongoose.connect('mongodb://localhost:27017/fullstack_db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db=mongoose.connection;
+db.on('error', (error)=>console.log(error));
+db.once('open', ()=>console.log('Database Connected'));
+
+app.use(cors());
+app.use(express.json());
+app.use(userRoute);
+ 
+
+app.listen(5000, () => console.log("Server up and running . . ."));
